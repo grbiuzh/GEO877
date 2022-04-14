1eccb4d2b280953dc8cb606d5731492122acc286
grbiuzh <gregory.biland@uzh.ch>
2022-04-14 13:50:28 +0200

    Updates

--- Gazetteers.py
+++ Gazetteers.py
@@ -6,5 +6,7 @@ import nltk as nlp
 import geopandas as gpd
 
 
+# Link to european gazetteer: https://www.mapsforeurope.org/access-data
+#It is to large to share on Github
 data = gpd.read_file("/Users/chaualala/Desktop/UZH/MSc Geographie/2. Semester/GEO877 - Spatial Algorithms/GEO877/open-gazetteer-gpkg/data/open_regional_gazetteer_2021.gpkg")
 data.head()
