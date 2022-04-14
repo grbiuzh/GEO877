97f7fb109b157ef730a6760e1970b77525632df5
grbiuzh <gregory.biland@uzh.ch>
2022-04-14 13:49:18 +0200

    Update

--- Gazetteers.py
+++ Gazetteers.py
@@ -1,1 +1,10 @@
-# 
+import geocoder as geo #Geonames gazetteer
+import numpy as np
+import matplotlib as mat
+import nltk as nlp
+
+import geopandas as gpd
+
+
+data = gpd.read_file("/Users/chaualala/Desktop/UZH/MSc Geographie/2. Semester/GEO877 - Spatial Algorithms/GEO877/open-gazetteer-gpkg/data/open_regional_gazetteer_2021.gpkg")
+data.head()
